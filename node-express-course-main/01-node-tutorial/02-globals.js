6e5c05cfb4ff6309e46480e5a66eb40fbb7a0929
L0wPressu7e <L0wPressu7e@gmail.com>
2021-08-15 00:04:11 +0300

    added tutorial files for the reference + move my own tutorial files into separate folder "

--- node-express-course-main/01-node-tutorial/02-globals.js
+++ node-express-course-main/01-node-tutorial/02-globals.js
@@ -6,4 +6,7 @@
 // module     - info about current module (file)
 // process    - info about env where the program is being executed
 
-const dir = __dirname;
+console.log(__dirname)
+setInterval(() => {
+  console.log('hello world')
+}, 1000)
