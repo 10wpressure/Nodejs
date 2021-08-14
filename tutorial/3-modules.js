d353f936c34e4cef746709ff6e48b51840e8cd72
L0wPressu7e <L0wPressu7e@gmail.com>
2021-08-15 00:58:50 +0300

    finished the basics

--- tutorial/3-modules.js
+++ tutorial/3-modules.js
@@ -1,11 +1,13 @@
 // CommonJS, every file is a module (by default)
 // Modules - Encapsulated Code (only share minimum)
 // import variables/constants:
-// either destructure, or use . notation
-//const names = require("./4-names");
+//    either destructure, or use . notation
+
+// const names = require("./4-names");
 const { john, peter } = require("./4-names");
 const sayHi = require("./5-utils");
-
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
 sayHi("susan");
 sayHi(john);
 sayHi(peter);
