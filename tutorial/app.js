8c66d7f5d677929c15e56e5dbe20afe2e6c8084c
L0wPressu7e <L0wPressu7e@gmail.com>
2021-08-15 23:01:32 +0300

    npm usage + gitignore

--- tutorial/app.js
+++ tutorial/app.js
@@ -1,16 +1,5 @@
-const http = require("http");
+const _ = require("lodash");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Welcome to our homepage");
-  } else if (req.url === "/about") {
-    res.end("Here is our short history");
-  } else {
-    res.end(`
-  <h1>Oops!</h1>
-  <p>We can't seem to find a page you are looking for</p>
-  <a href='/'>back home</a>`);
-  }
-});
-
-server.listen(5000);
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items);
+console.log(newItems);
