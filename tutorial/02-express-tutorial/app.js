109d8dadce6e8260db63dea2eda604ab1d6ea035
L0wPressu7e <L0wPressu7e@gmail.com>
2021-08-24 22:45:29 +0300

    query strings

--- tutorial/02-express-tutorial/app.js
+++ tutorial/02-express-tutorial/app.js
@@ -22,7 +22,7 @@ app.get('/api/products/:productID', (req, res) => {
     (product) => product.id === Number(productID),
   );
   if (!singleProduct) {
-    return res.status(404).send(`Product Does Not Exist Блядь`);
+    return res.status(404).send(`Product Does Not Exist`);
   }
   res.json(singleProduct);
 });
@@ -30,6 +30,26 @@ app.get('/api/products/:productID', (req, res) => {
 app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
   console.log(req.params);
   res.send('hello world');
+});
+
+app.get('/api/v1/query', (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  if (sortedProducts.length < 1) {
+    return res.status(200).json({ success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
