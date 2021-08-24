30af4035b601e6e0c8196a602685e307073ff3e1
L0wPressu7e <L0wPressu7e@gmail.com>
2021-08-24 23:40:37 +0300

    express and third party middleware, morgan logger

--- tutorial/02-express-tutorial/app.js
+++ tutorial/02-express-tutorial/app.js
@@ -1,21 +1,33 @@
 const express = require('express');
 app = express();
+const morgan = require('morgan');
+
+//middleware
+const logger = require('./logger');
+const authorize = require('./authorize');
+
 port = 5000;
 // req => middleware => res
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
+// 1. use vs route
+// 2. options - our own / express / third party
 
-app.get('/', logger, (req, res) => {
+// app.use([logger, authorize]);
+// app.use(express.static('./public'));
+app.use(morgan('tiny'));
+
+app.get('/', (req, res) => {
   res.send('Home');
 });
-app.get('/about', logger, (req, res) => {
+app.get('/about', (req, res) => {
   res.send('About');
+});
+app.get('/api/products', (req, res) => {
+  res.send('Products');
+});
+app.get('/api/items', (req, res) => {
+  console.log(req.user);
+  res.send('Items');
 });
 
 app.listen(port, () => {
