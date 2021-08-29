aeca6bdfdb38bca2362a3ae9e9f495ac59376e45
L0wPressu7e <L0wPressu7e@gmail.com>
2021-08-29 19:23:24 +0300

    postman

--- tutorial/02-express-tutorial/app.js
+++ tutorial/02-express-tutorial/app.js
@@ -1,33 +1,77 @@
 const express = require('express');
 app = express();
-const morgan = require('morgan');
-
-//middleware
-const logger = require('./logger');
-const authorize = require('./authorize');
-
 port = 5000;
-// req => middleware => res
+let { people } = require('./data');
 
-// 1. use vs route
-// 2. options - our own / express / third party
+// static assets
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+// get data from server
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people });
+});
+// add data to the server
+app.post('/api/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' });
+  }
+  res.status(201).json({ success: true, person: name });
+});
 
-// app.use([logger, authorize]);
-// app.use(express.static('./public'));
-app.use(morgan('tiny'));
+app.post('/api/postman/people', (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: 'please provide name value' });
+  }
+  res.status(201).send({ success: true, data: [...people, name] });
+});
 
-app.get('/', (req, res) => {
-  res.send('Home');
+app.post('/login', (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome, ${name}!`);
+  }
+  res.status(401).send('Please, enter the valid credentials');
 });
-app.get('/about', (req, res) => {
-  res.send('About');
+// change data on a server
+app.put('/api/people/:id', (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
 });
-app.get('/api/products', (req, res) => {
-  res.send('Products');
-});
-app.get('/api/items', (req, res) => {
-  console.log(req.user);
-  res.send('Items');
+// delete data on a server
+app.delete('/api/people/:id', (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  }
+  const newPeople = people.filter(
+    (person) => person.id !== Number(req.params.id),
+  );
+  return res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(port, () => {
