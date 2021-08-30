f9b8166159d456da9e0727c4a55d85db4d4269ec
L0wPressu7e <L0wPressu7e@gmail.com>
2021-08-31 01:13:06 +0300

    mongodb

--- tutorial/03-task-manager/starter/app.js
+++ tutorial/03-task-manager/starter/app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 app = express();
 
+const tasks = require('./routes/tasks');
+
+// middleware
+app.use(express.json());
+
 // routes
 app.get('/hello', (req, res) => {
   res.send('Task Manager App');
 });
 
+app.use('/api/v1/tasks', tasks);
+
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on the port ${port}...`));
+app.listen(
+  port,
+  `localhost`,
+  444,
+  console.log(`Server is listening on the port ${port}...`),
+);
---
+++ tutorial/03-task-manager/starter/db/connect.js
@@ -0,0 +1,2 @@
+const connectionString =
+  'mongodb+srv://low_pressure:<password>@nodeexpressprojects.g47rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
