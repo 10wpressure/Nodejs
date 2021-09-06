b50821e805901c1ba045a06416183f6f05198d5d
L0wPressu7e <66515502+L0wPressu7e@users.noreply.github.com>
2021-09-06 23:50:13 +0300

    query objects

---
+++ tutorial/04-store-api/starter/controllers/products.js
@@ -0,0 +1,27 @@
+const Product = require('../models/product');
+
+const getAllProductsStatic = async (_req, res, _next) => {
+  const products = await Product.find({
+    page: '2',
+  });
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res, _next) => {
+  const { featured, company } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
