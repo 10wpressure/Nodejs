7dd5878fcc1b9c57432dde4f61c4c55aa7f64575
L0wPressu7e <L0wPressu7e@gmail.com>
2021-09-07 23:29:04 +0300

    query operators

--- tutorial/04-store-api/starter/controllers/products.js
+++ tutorial/04-store-api/starter/controllers/products.js
@@ -1,20 +1,25 @@
 const Product = require('../models/product');
 
 const getAllProductsStatic = async (_req, res, _next) => {
+  const search = 'ab';
   const products = await Product.find({
-    page: '2',
+    name: { $regex: search, $options: 'i' },
   });
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res, _next) => {
-  const { featured, company } = req.query;
+  const { featured, company, name } = req.query;
   const queryObject = {};
+
   if (featured) {
     queryObject.featured = featured === 'true' ? true : false;
   }
   if (company) {
     queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' };
   }
   console.log(queryObject);
   const products = await Product.find(queryObject);
