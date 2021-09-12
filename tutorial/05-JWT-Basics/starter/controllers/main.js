2e194035de3daccecc891bffc73edfe094cac69c
L0wPressu7e <L0wPressu7e@gmail.com>
2021-09-12 16:46:45 +0300

    todo added,token is received

--- tutorial/05-JWT-Basics/starter/controllers/main.js
+++ tutorial/05-JWT-Basics/starter/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const CustomAPIError = require('../errors/custom-error');
 
 const login = async (req, res) => {
@@ -10,10 +11,19 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomAPIError('Please provide email and password', 400);
   }
-  console.log(username, password);
-  res.send('Fake Login/Register/Signup');
+
+  // just for DEMO, normally provided by db
+  const id = new Date().getDate();
+
+  // payload, try to keep it small, better for the slow connections
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 const dashboard = async (req, res) => {
+  console.log(req.headers )
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
